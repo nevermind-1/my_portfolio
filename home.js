a97babe16193007b58694a3cbddeaf4a23f9cf1c
Ajuba Okwuchukwu Anthony <ajubaokwuchukwuanthony2000@gmail.com>
2023-10-13 09:47:03 +0100

    modified

--- home.js
+++ home.js
@@ -126,3 +126,12 @@ console.log("what the fuck is happening in this part of the code? The function i
 //   }
 // }
 
+
+// // preloader
+
+// var preloader = document.getElementById('preloader');
+
+// window.addEventListener("load", function(){
+//   preloader.style.display = "none";
+// });
+
